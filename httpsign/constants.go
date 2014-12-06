a73267726b601cd70866b03538ca1ce4d4e77dc0
Russell Jones <rjones@mailgunhq.com>
2014-12-06 02:39:04 +0000

    Increase timeout period. Previously it was only 30-20 = 10 seconds. This commit boosts it to 100-5 = 95 seconds.

--- httpsign/constants.go
+++ httpsign/constants.go
@@ -1,8 +1,8 @@
 package httpsign
 
-const MaxSkewSec = 20                     // 20 sec
-const CacheTimeout = 30                   // 30 sec
-const CacheCapacity = 5000 * CacheTimeout // 5,000 msg/sec * 30 sec = 150,000 elements
+const MaxSkewSec = 5                      // 5 sec
+const CacheTimeout = 100                  // 100 sec
+const CacheCapacity = 5000 * CacheTimeout // 5,000 msg/sec * 100 sec = 500,000 elements
 
 const XMailgunSignature = "X-Mailgun-Signature"
 const XMailgunSignatureVersion = "X-Mailgun-Signature-Version"
